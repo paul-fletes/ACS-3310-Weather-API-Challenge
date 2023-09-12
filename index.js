77c2e8ca6acaf6ce2cf9682ebca1ceb6d4541e53
Paul Fletes <paul.fletes@gmail.com>
2023-09-11 23:02:15 -0500

    complete challenges

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
-function getWeather(apiKey, zip, callback) {
-  const apiKey = apiKey
-  const units = 'imperial'
-  const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
-  fetch(path)
-    .then(res => res.json())
-    .then(json => {
-      callback(json)
-    })
-    .catch(err => console.log(err.message))
+export async function getWeatherByZip(apiKey, zip, units = 'imperial') {
+  const path = `zip=${zip}&appid=${apiKey}&units=${units}`;
+  return fetchAndProcessWeatherData(path);
 }
 
-export default getWeather;
+export async function getWeatherByCity(apiKey, city, state, country, units = 'imperial') {
+  let path = `q=${city}`;
+  if (state) {
+    path += `,${state}`;
+  }
+  if (country) {
+    path += `,${country}`;
+  }
+  path += `&appid=${apiKey}&units=${units}`;
+  return fetchAndProcessWeatherData(path);
+}
+
+export async function getWeatherByGeo(apiKey, lat, lon, units = 'imperial') {
+  const path = `lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
+  return fetchAndProcessWeatherData(path);
+}
+
+async function fetchAndProcessWeatherData(pathEnd) {
+  let path = 'https://api.openweathermap.org/data/2.5/weather?'
+  path += pathEnd
+  const res = await fetch(path);
+  const json = await res.json();
+  const { main, weather } = json;
+  const { temp, pressure, humidity, temp_min, temp_max } = main;
+  const { description, icon } = weather[0];
+
+  return { temp, pressure, humidity, temp_min, temp_max, description, icon };
+}
